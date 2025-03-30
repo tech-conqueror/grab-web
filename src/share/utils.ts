aea348e9297b444718354a1d9e4bec48293982e3
tech-conqueror <mannm.engineer@gmail.com>
2025-03-30 15:54:12 +0700

    feat: stimulate multiple cars

--- src/share/utils.ts
+++ src/share/utils.ts
@@ -12,3 +12,9 @@ Number.prototype.round = function (places: number): number {
 // Wait function with proper TypeScript typing
 export const wait = (t: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, t));
+
+export const getRandomInt = (min: number, max: number): number => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
